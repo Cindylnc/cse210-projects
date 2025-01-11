dd5cf0dafed3009015162e4b6d31e5281f6a6cde
Cindylnc <naj22003@byui.edu>
2025-01-11 14:32:34 -0700

    Exercise 4 complete

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -5,5 +5,36 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+        Console.WriteLine ("Enter a list of numbers, type 0 when finished.");
+        Console.Write ("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        List<int> numbers = new List<int>();
+      
+        int sum = 0;
+        /*int count = 0;*/
+
+        while (number != 0)
+        {        
+            numbers.Add(number);
+            sum += number;
+            /*count++;*/
+
+            Console.Write("Enter a number: ");
+            number = int.Parse(Console.ReadLine());
+        }
+
+        float average = ((float)sum) / numbers.Count;
+        int largestNumber = numbers.Max();
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largestNumber}");
+        
+       
     }
+
+    
+
 }
