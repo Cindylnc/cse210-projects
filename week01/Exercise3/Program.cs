ee4bebb1b66cc9297fbd8a0ca8c4d9562a4c3237
Cindylnc <naj22003@byui.edu>
2025-01-11 11:27:32 -0700

    Exercise 3 Step 1

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,34 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        Console.Write("What is the magic number? ");
+        string response = Console.ReadLine();
+        int magicNumber = int.Parse(response);
+
+        Console.Write ("What is your guess? ");
+        string guess = Console.ReadLine();
+        int userGuess = int.Parse(guess);
+
+        if (userGuess > magicNumber)
+        {
+            Console.WriteLine ("Lower");
+        }
+        else if (userGuess < magicNumber)
+        {
+            Console.WriteLine ("Higher");
+        }
+        else
+        {
+            Console.WriteLine ("You guessed it!");
+        }
+    
+
+
+
+
+
+
+
     }
 }
