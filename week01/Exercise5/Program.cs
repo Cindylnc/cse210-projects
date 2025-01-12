7eaa41e663950244741326e2c7c1c51fc030fd9b
Cindylnc <naj22003@byui.edu>
2025-01-11 23:55:47 -0700

    excercise 5 complete

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -5,5 +5,67 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
+
+        /*static void Message()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+        Message();
+
+
+        static string DisplayName ()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine ();
+
+            return name;
+        }
+        DisplayName();*/
+
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+        DisplayWelcome();
+
+        {  
+
+            string userName = PromptUserName();
+            int userNumber = PromptUserNumber();
+
+            int squaredNumber = SquareNumber(userNumber);
+
+            DisplayResult(userName, squaredNumber);
+        }
+
+
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+
+            return name;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            int favNumber = int.Parse(Console.ReadLine());
+
+            return favNumber;
+        }
+
+        static int SquareNumber(int number)
+        {
+            int square = number * number;
+            return square;
+        }
+
+        static void DisplayResult(string name, int square)
+        {
+            Console.WriteLine($"{name}, the square of your number is {square}");
+        }
+
+
     }
 }
