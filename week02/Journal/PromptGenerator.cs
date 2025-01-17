98c250aca1dc78e56e4d1ceafb0420a6f217e289
Cindylnc <naj22003@byui.edu>
2025-01-16 23:04:58 -0700

    update questions

--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -6,14 +6,15 @@ public class PromptGenerator
         "What is something you are grateful for today that you often overlook?",
         "What was the best part of my day?",
         "How did I see the hand of the Lord in my life today?",
-        "What was the strongest emotion I felt today?",
-        "If I had one thing I could do over today, what would it be?",
-        "What is one thing that made you smile today?",
-        "What challenge did you face today, and how did you handle it?",
         "What is something you learned today?",
         "What is one moment today that brought you peace or calm?",
         "How did you connect with others today, even in small ways?",
         "How did you grow or stretch outside your comfort zone today?",
+        "What was the strongest emotion I felt today?",
+        "If I had one thing I could do over today, what would it be?",
+        "What is one thing that made you smile today?",
+        "What challenge did you face today, and how did you handle it?",
+        "Who were you the most happy seeing today?",
     };
 
     public string GetRandomPrompt()
