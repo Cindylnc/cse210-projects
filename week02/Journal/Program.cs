bcaa901c171fb5a52ac3cc9c2422ec4a1938c284
Cindylnc <naj22003@byui.edu>
2025-01-16 23:02:48 -0700

    random prompt

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,60 @@
 using System;
+using System.Diagnostics;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Journal Project.");
+     
+        PromptGenerator ooo = new PromptGenerator();
+        string randomPrompt = ooo.GetRandomPrompt();
+        Console.WriteLine(randomPrompt);
+
+
+        Console.WriteLine("\nWelcome to the Journal Program!");
+        string menu;
+        do
+        {
+            Console.WriteLine("Please select one of the following choices");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do? ");
+            menu = Console.ReadLine();
+            if (menu == "1")
+            {
+
+            }
+            else if (menu == "2")
+            {
+
+            }
+            else if (menu == "3")
+            {
+
+            }
+            else if (menu == "4")
+            {
+
+            }
+            else if (menu == "5")
+            {
+                Console.WriteLine("See ya next time!");
+            }
+            else
+            {
+                Console.WriteLine("That's not an option");
+            }
+            Console.WriteLine();
+
+
+        }
+        while (menu !="5");
+
+
     }
 }
--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -1,9 +1,25 @@
 public class PromptGenerator
 {
-    public List<string> _prompts;
+    public List<string> _prompts = new List<string>()
+    {
+        "Who was the most interesting person I interacted with today?",
+        "What is something you are grateful for today that you often overlook?",
+        "What was the best part of my day?",
+        "How did I see the hand of the Lord in my life today?",
+        "What was the strongest emotion I felt today?",
+        "If I had one thing I could do over today, what would it be?",
+        "What is one thing that made you smile today?",
+        "What challenge did you face today, and how did you handle it?",
+        "What is something you learned today?",
+        "What is one moment today that brought you peace or calm?",
+        "How did you connect with others today, even in small ways?",
+        "How did you grow or stretch outside your comfort zone today?",
+    };
 
     public string GetRandomPrompt()
     {
-        return "";
+        Random random = new Random();
+        int randomIndex = random.Next(_prompts.Count);
+        return _prompts[randomIndex];
     }
 }
