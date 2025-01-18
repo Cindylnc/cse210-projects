8defc3aff9b8f592de1784d5e9d783f42a941d75
Cindylnc <naj22003@byui.edu>
2025-01-17 20:58:08 -0700

    menu if statements added

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Diagnostics;
+using System.IO.Enumeration;
 using System.Threading.Tasks.Dataflow;
 
 class Program
@@ -11,28 +12,13 @@ class Program
 
         //Create prompGenerator object
         PromptGenerator promptGenerator = new PromptGenerator();
-        string randomPrompt = promptGenerator.GetRandomPrompt();
-        Console.WriteLine(randomPrompt);
-        string userResponse = Console.ReadLine();
-        
-        //Get the current Date
-        DateTime theCurrentDate = DateTime.Now;
-        string dateText = theCurrentDate.ToShortDateString();
-
-        //Create a new Entry object
-        Entry newEntry = new Entry
-        {
-            _date = dateText,
-            _promptText = randomPrompt,
-            _entryText = userResponse,
-        };
 
         //Create journal object
         Journal journal = new Journal ();
-        journal.AddEntry(newEntry);
-        
 
         
+
+
         Console.WriteLine("\nWelcome to the Journal Program!");
         string menu;
         do
@@ -40,26 +26,53 @@ class Program
             Console.WriteLine("Please select one of the following choices");
             Console.WriteLine("1. Write");
             Console.WriteLine("2. Display");
-            Console.WriteLine("3. Load");
-            Console.WriteLine("4. Save");
+            Console.WriteLine("3. Save");
+            Console.WriteLine("4. Load");
             Console.WriteLine("5. Quit");
             Console.Write("What would you like to do? ");
             menu = Console.ReadLine();
+
+            //WRITE
             if (menu == "1")
             {
+                string randomPrompt = promptGenerator.GetRandomPrompt();
+                Console.WriteLine(randomPrompt);
+                string userResponse = Console.ReadLine();
 
+                //Get the current Date
+                DateTime theCurrentDate = DateTime.Now;
+                string dateText = theCurrentDate.ToShortDateString();
+
+                //Create a new Entry object
+                Entry newEntry = new Entry
+                {
+                    _date = dateText,
+                    _promptText = randomPrompt,
+                    _entryText = userResponse,
+                };
+                journal.AddEntry(newEntry);
             }
+
+            //DISPLAY
             else if (menu == "2")
             {
-
+                journal.DisplayAll();
             }
+
+            //SAVE
             else if (menu == "3")
             {
-
+                Console.WriteLine("Enter the filename to save the journal:");
+                string fileName = Console.ReadLine();
+                journal.SaveToFile(fileName);
             }
+
+            //LOAD
             else if (menu == "4")
             {
-
+                Console.WriteLine("Enter the filename to load the entries from:");
+                string fileName = Console.ReadLine();
+                journal.LoadFromFile(fileName);
             }
             else if (menu == "5")
             {
