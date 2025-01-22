fec5d509cbce45f05c6056706058d6f9b9a22e8f
Cindylnc <naj22003@byui.edu>
2025-01-21 21:22:52 -0700

    creating classes

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,27 @@
+public class Scripture{
+    private Reference _reference;
+    private List<Word> _words = new List<Word>();
+
+    public Scripture (Reference reference, string text)
+    {
+
+    }
+    public void HideRandomWords (int numberToHide)
+    {
+
+    }
+    public string GetDisplayTest ()
+    {
+        return "";
+    }
+    public bool IsCompletelyHidden()
+    {
+        return false;
+    }
+
+
+    
+
+
+
+}
