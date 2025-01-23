c675a90b1f9ef26c9336dc75c9e4830c52700b48
Cindylnc <naj22003@byui.edu>
2025-01-22 22:36:05 -0700

    reference class

--- week03/ScriptureMemorizer/Reference.cs
+++ week03/ScriptureMemorizer/Reference.cs
@@ -5,12 +5,19 @@ public class Reference { // Keeps track of the book, chapter, and verse informat
     private int _endVerse;
 
 
-    public Reference (string book, int chapter, int verse)
+    public Reference (string book, int chapter, int verse) //single verse
     {  //Decide what to set _endVerse equal to
-
+        _book = book;
+        _chapter = chapter;
+        _verse = verse;
+        _endVerse = verse;
     }
-    public Reference (string book, int chapter, int startVerse, int endVerse)  
+    public Reference (string book, int chapter, int startVerse, int endVerse)  //more than one verse
     {
+        _book = book;
+        _chapter = chapter;
+        _verse = startVerse;
+        _endVerse = endVerse;
 
     }
 
