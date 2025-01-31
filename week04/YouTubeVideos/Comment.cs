8dcb91fed38da787208b4205bc13afd86022466c
Cindylnc <naj22003@byui.edu>
2025-01-30 21:23:44 -0700

    classes added

---
+++ week04/YouTubeVideos/Comment.cs
@@ -0,0 +1,15 @@
+public class Comment
+{
+    private string _name;
+    private string _text;
+
+    public Comment (string name, string text){
+
+    }
+    
+    public string Display()
+    {
+        return _name;
+    }
+
+}
---
+++ week04/YouTubeVideos/Video.cs
@@ -0,0 +1,27 @@
+public class Video 
+{
+    private string _title;
+    private string _author;
+    private int _length;
+    private List<Comment> _comments;
+
+
+    public Video(string title, string author, int length)
+    {
+
+    }
+
+    public void AddComment(Comment comment)
+    {
+
+    }
+    public int GetNumberOfComments()
+    {
+        return _comments.Count;
+    }
+    public void Diplay()
+    {
+        
+    }
+
+}
