31a91720804a67a335dcca31512013e7a5129587
Cindylnc <naj22003@byui.edu>
2025-02-07 16:55:28 -0700

    testing activity

--- week05/Mindfulness/Activity.cs
+++ week05/Mindfulness/Activity.cs
@@ -12,22 +12,57 @@ public class Activity
 
     public void DisplayStartingMessage()
     {
-        _name = "";
+       Console.Clear();
+       Console.WriteLine($"Starting {_name}...");
+       Console.WriteLine(_description);
+       Console.WriteLine("Enter duration (in seconds): ");
+
+       _duration = int.Parse(Console.ReadLine());
+       Console.WriteLine("Prepare to begin..");
+       ShowSpinner(10);
+
     }
 
     public void DisplayEndingMessage() 
     {
-
+        Console.WriteLine("Good Job!");
+        ShowSpinner(3);
+        Console.WriteLine($"You have completed the {_name} for {_duration}seconds.");
     }
 
     public void ShowSpinner(int seconds)
     {
+        for (int i = 0; i < seconds; i++)
+        {
+            Console.WriteLine("/");
+            Thread.Sleep(300);
+            Console.Write("\b \b");
 
+            Console.WriteLine("-");
+            Thread.Sleep(300);
+            Console.Write("\b \b");
+
+            Console.WriteLine("\\");
+            Thread.Sleep(300);
+            Console.Write("\b \b");
+
+            Console.WriteLine("|");
+            Thread.Sleep(300);
+            Console.Write("\b \b");
+        }
+        
     }
 
 
     public void ShowCountDown(int seconds)
     {
+        for (int i=seconds; i > 0; i--)
+        {
+            Console.WriteLine(i + "");
+            Thread.Sleep(1000);
+            Console.Write("\b\b");
+        }
+        Console.WriteLine();
 
     }
 
