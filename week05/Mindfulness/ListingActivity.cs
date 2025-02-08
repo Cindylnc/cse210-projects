1f3632df2c773aaf78d7edc55614c5235fe209f2
Cindylnc <naj22003@byui.edu>
2025-02-07 17:17:57 -0700

    listing activity

--- week05/Mindfulness/ListingActivity.cs
+++ week05/Mindfulness/ListingActivity.cs
@@ -1,26 +1,46 @@
 public class ListingActivity : Activity
 {
-    private int _count;
-    private List<prompt> _prompts;
-
-    public ListingActivity(int count)
+    //private int _count;
+    private List<string> _prompts = new List<string>
     {
+        "Who are people that you appreciate?",
+        "What are personal strengths of yours?",
+        "Who are some of your personal heroes?"
+    };
 
-    }
+    public ListingActivity()
+        : base("Listing Activity", "This activity helps you reflect on the good things in life by listing them")
+        {}
+
     public void Run()
     {
+        DisplayStartingMessage();
+        Console.WriteLine("Prompt: " + GetRandomPrompt());
+        ShowSpinner(3);
 
+        Console.WriteLine("Start listing items:");
+        DateTime endTime = DateTime.Now.AddSeconds(_duration);
+        int count = 0;
+
+        while (DateTime.Now < endTime)
+        {
+            Console.Write("> ");
+            Console.ReadLine();
+            count++;
+        }
+
+        Console.WriteLine($"You listed {count} items.");
+        DisplayEndingMessage();
     }
 
-    public void GetRandomPrompt()
+    public string GetRandomPrompt()
     {
-
+        Random random= new Random();
+        return _prompts[random.Next(_prompts.Count)];
     }
 
-    public List<prompt> GetListFromUser()
-    {
-        
-    }
+    //public List<prompt> GetListFromUser()
+    //{}
 
 
 }
