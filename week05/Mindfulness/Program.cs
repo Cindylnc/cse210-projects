0ca465ed0885ee9dda8b34c89e91c853d1ba5477
Cindylnc <naj22003@byui.edu>
2025-02-07 19:05:23 -0700

    adding while statement

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -8,13 +8,37 @@ class Program
         Console.WriteLine("Hello World! This is the Mindfulness Project.");
         Console.WriteLine("");
 
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("MINDFULNESS ACTIVITIES");
+            Console.WriteLine("1. Breathing Activity");
+            Console.WriteLine("2. Reflecting  Activity");
+            Console.WriteLine("3. Listing Activity");
+            Console.WriteLine("4. Exit");
+            Console.WriteLine("Choose an option: ");
 
-        Console.WriteLine("Mindfulness Activities");
-        Console.WriteLine("1. Breathing Activity");
-        Console.WriteLine("2. Reflectio  Activity");
-        Console.WriteLine("3. Listing Activity");
-        Console.WriteLine("4. Exit");
-        Console.WriteLine("Choose an option: ");
-
+            string choice = Console.ReadLine();
+            if (choice =="1")
+            {
+                new BreathingActivity().Run();
+            }
+            else if (choice =="2")  
+            {
+                new ReflectingActivity().Run();
+            }
+            else if (choice =="3")  
+            {
+                new ListingActivity().Run();
+            }
+            else if (choice == "4")
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Invalid option. Try again");
+            }
+        }
     }
 }
