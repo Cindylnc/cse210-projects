ce9dc9d9ba63f64bc4fe92a34a28b55154003a97
Cindylnc <naj22003@byui.edu>
2025-02-07 17:01:42 -0700

    breathin activity run

--- week05/Mindfulness/BreathingActivity.cs
+++ week05/Mindfulness/BreathingActivity.cs
@@ -1,13 +1,26 @@
+using System.Diagnostics.CodeAnalysis;
+
 public class BreathingActivity : Activity
 {
     public BreathingActivity()
-        : base("Breathing Activity": "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.")
-        {}
+        : base("Breathing Activity", "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing."){}
 
-    
 
     public void Run()
     {
+        DisplayStartingMessage();
+
+        DateTime endTime = DateTime.AddSeconds(_duration);
+        while (DateTime.Now < endTime)
+        {
+            Console.WriteLine("Breathe in...");
+            ShowCountDown(3);
+
+            Console.WriteLine("Breathe out...");
+            ShowCountDown(3);
+        }
+
+        DisplayEndingMessage();
 
     }
 }
