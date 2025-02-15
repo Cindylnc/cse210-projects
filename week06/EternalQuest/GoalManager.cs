1764afa8a0be63cb2aab59b51a561c30fc929d26
Cindylnc <naj22003@byui.edu>
2025-02-15 09:19:53 -0700

    GoalManager started

---
+++ week06/EternalQuest/GoalManager.cs
@@ -0,0 +1,46 @@
+public class GoalManager
+{
+    List<Goal> goals = new List<Goal>();
+    private int _score;
+
+    public GoalManager(int score)
+    {
+        _score = score;
+    }
+
+    public void Start()
+    {
+
+    }
+    public void DisplayPlayerInfo()
+    {
+
+    }
+    public void ListGoalNames ()
+    {
+
+    }
+    public void ListGoalDetails ()
+    {
+
+    }
+    public void CreateGoal() 
+    {
+
+    }
+    public void RecordEvent()
+    {
+
+    }
+    public void SaveGoals()
+    {
+
+    }
+    public void LoadGoals()
+    {
+        
+    }
+
+
+
+}
