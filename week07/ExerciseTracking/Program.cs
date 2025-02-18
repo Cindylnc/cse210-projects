3af876eedda2bb63b3334bfe6e0d1bc55c0d4590
Cindylnc <naj22003@byui.edu>
2025-02-18 15:58:23 -0700

    programs code

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -5,5 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        Console.WriteLine("");
+
+        List<Activity> activites = new List<Activity>
+        {
+            new Running ("18 Feb 2022", 30, 3.0),
+            new Biking ("15 Feb 2025", 30, 12.0),
+            new Swimming("15 Feb 2025", 35, 20)
+        };
+
+        foreach (var activity in activites)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
