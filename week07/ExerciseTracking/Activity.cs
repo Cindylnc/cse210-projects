8ab36f9eeaea9090e2da8ed03fd18c098e827710
Cindylnc <naj22003@byui.edu>
2025-02-18 15:23:06 -0700

    Activity class created

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,27 @@
+public abstract class Activity
+{
+    protected string _date;
+    protected int _lenght;
+
+    public Activity (string date, int lenght)
+    {
+        _date = date;
+        _lenght = lenght;
+    }
+
+    public string GetDate()
+    {
+        return _date;
+    }
+
+    public int Getlenght()
+    {
+        return _lenght;
+    }
+    
+
+    public abstract int GetDistance();
+    public abstract int GetSpeed();
+    public abstract int GetNumberofLamps();
+
+}
